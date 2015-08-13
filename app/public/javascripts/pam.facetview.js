c2a740ff4e2bced4833e85d40939e9d1a3b90730
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-08-13 16:22:39 +0300

    Refs #27983; Updated listing table and facets

--- app/public/javascripts/pam.facetview.js
+++ app/public/javascripts/pam.facetview.js
@@ -24,10 +24,6 @@ function replaceNumbers(){
     });
 }
 
-function addHeaders(){
-    $("#facetview_results").append("<thead><tr><th>Country</th><th>Link to EU Emissions Trading Scheme (ETS)</th><th>Targeted sectors</th><th>Name</th><th>Status</th><th>Projection scenario in which the PAM is included</th><th>Total GHG savings by 2020 (in kt CO2 equivalent)</th></tr></thead>");
-}
-
 function fixDataTitles(){
     var th_list = [];
     $("#facetview_results thead th").each(function(idx, th){
@@ -42,101 +38,31 @@ function fixDataTitles(){
 }
 
 function viewReady(){
+    addHeaders("#facetview_results");
     replaceNumbers();
-    addHeaders();
     fixDataTitles();
 }
 
 jQuery(document).ready(function($) {
-    $('.facet-view-simple').facetview({
-        search_url: './api',
-        search_index: 'elasticsearch',
-        datatype: 'json',
-        initial_search: false,
-        enable_rangeselect: true,
-        pushstate : true,
-        rangefacets: [field_base + '2020_total_kt_CO2'],
-        post_init_callback: function() {
-          add_EEA_settings();
-        },
-        post_search_callback: function() {
-          add_EEA_settings();
-          viewReady();
-        },
-
-        facets: [
-            {'field':field_base + 'Country', 'display': 'Country', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Link_to_EU_Emissions_Trading_Scheme_ETS', 'display': 'Link to EU Emissions Trading Scheme (ETS)', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Targeted_sectors', 'display': 'Targeted sectors', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Affected_GHG', 'display': 'Affected GHG', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Status', 'display': 'Status', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Projection_scenario_in_which_the_PAM_is_included', 'display': 'Projection scenario in which the PAM is included', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Type_of_instrument', 'display': 'Type of instrument', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Related_EU_policies', 'display': 'Related EU Policies', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + 'Type_of_implementing_entities', 'display': 'Type of implementing entities', 'size':'50', 'order': 'term','facet_display_options': ['sort', 'checkbox']},
-            {'field':field_base + '2020_total_kt_CO2', 'display': 'Total GHG savings by 2020 (in kt CO2 equivalent)', 'size':'1600', 'order': 'term','facet_display_options': ['sort', 'checkbox']}
-        ],
-
-
-        result_display: [
-            [
-                {
-                    'field': field_base + "Country",
-                    'post': '</td>'
-                },
-                {
-                    'pre': '<td>',
-                    'field': field_base + "Link_to_EU_Emissions_Trading_Scheme_ETS",
-                    'post': '</td>'
-                },
-                {
-                    'pre': '<td>',
-                    'field': field_base + "Targeted_sectors",
-                    'post': '</td>'
-                },
-                {
-                    'pre': '<td><a href="./details?pamid=',
-                    'field':field_base + 'PAMID',
-                    'post': '">'
-                },
-
-                {
-                    'field': field_base + "Name",
-                    'post': '</a></td>'
-                },
-                {
-                    'pre': '<td>',
-                    'field':field_base + 'Status',
-                    'post': '</td>'
-                },
-
-                {
-                    'pre': '<td>',
-                    'field': field_base + "Projection_scenario_in_which_the_PAM_is_included",
-                    'post': '</td>'
-                },
-                {
-                    'pre': '<td>',
-                    'field':field_base + '2020_total_kt_CO2'
-                }
-            ]
-        ],
-
-        paging: {
-            from: 0,
-            size: 10
-        },
-        linkify: false,
-        pager_on_top: true
-
-
-    });
+    eea_facetview('.facet-view-simple', 
+        {
+            search_url: './api',
+            search_index: 'elasticsearch',
+            datatype: 'json',
+            initial_search: false,
+            enable_rangeselect: true,
+            enable_geoselect: true,
+            post_init_callback: function() {
+              add_EEA_settings();
+            },
+            post_search_callback: function() {
+              add_EEA_settings();
+              viewReady();
+            },
+            paging: {
+                from: 0,
+                size: 10
+            }
+        });
     replaceNumbers();
-
-/*    $('[id="' + field_base + '2020_total_kt_CO2"]').closest(".facetview_filter").addClass("facet_range_only");
-
-    $(".facet_range_only").delegate(".facetview_showtree", "click", function(event){
-        var tmp_facet = $(event.target).closest(".facetview_filter");
-        tmp_facet.find("a.facetview_facetrange").click();
-    });*/
 });
