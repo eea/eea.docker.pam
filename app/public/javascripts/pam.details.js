5bbbd00f541056d21c2bce82134870d68417a59d
Zoltan Szabo <zoltan.szabo@eaudeweb.ro>
2015-08-13 17:50:56 +0300

    Refs #27983; Started implementing the customizations for the detail page

---
+++ app/public/javascripts/pam.details.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function($) {
+    jQuery("<p>").text("Annual savings [kt CO sub 2 equivalent per year]").insertAfter(jQuery(".section5_container h2"));
+    jQuery("<p>").text("Annual savings [kt CO sub 2 equivalent per year]").insertAfter(jQuery(".section6_container h2"));
+});
