09f92d53b839277c5689b7100a262f89c8e603a5
DragosChirila <dragos.chirila@eaudeweb.ro>
2016-07-27 14:22:19 +0200

    Clean up and refactoring.
    refs #73715

--- app/public/javascripts/pam.facetview.js
+++ app/public/javascripts/pam.facetview.js
@@ -36,45 +36,6 @@ function fixHeights(){
     });
 }
 
-function replaceNumbers(){
-    var possibleContainers = ['a', 'td', 'th'];
-    var chemsMapping = {'CH4':'CH<sub>4</sub>',
-                        'CO2':'CO<sub>2</sub>',
-                        'N2O':'N<sub>2</sub>O',
-                        'SF6':'SF<sub>6</sub>'};
-    jQuery.each(possibleContainers, function(idx, container){
-        var elems = jQuery(container);
-        jQuery.each(elems, function(idx, elem){
-            if ((jQuery(elem).children().length === 0) || (container === 'a')){
-                var shouldReplace = false;
-                var replacedText = jQuery(elem).html();
-                jQuery.each(chemsMapping, function(key, value){
-                    if (replacedText.indexOf(key) !== -1){
-                        replacedText = replacedText.replace(key, value);
-                        shouldReplace = true;
-                    }
-                });
-                if (shouldReplace){
-                    jQuery(elem).html(replacedText);
-                }
-            }
-        });
-    });
-}
-
-function fixDataTitles(){
-    var th_list = [];
-    $("#facetview_results thead th").each(function(idx, th){
-        th_list.push($(th).text());
-
-    })
-    $("#facetview_results tr").each(function(tr_idx, tr){
-        $(tr).find("td").each(function(td_idx, td){
-            $(td).attr("data-title", th_list[td_idx]);
-        });
-    });
-}
-
 function removeMissingDetails(){
     $.each($(".details_link"), function(idx, link){
         href = $(link).attr("href");
